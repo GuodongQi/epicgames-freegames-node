90578b45b74096c7710007b6cb857d954fa95d8b
guodong qi <guodong_qi@zju.edu.cn>
2023-10-13 15:40:26 +0800

    add https ssl

--- src/common/server.ts
+++ src/common/server.ts
@@ -23,7 +23,10 @@ export async function createServer(): Promise<http.Server> {
     Object.entries(config.webPortalConfig?.serverOpts).length > 0
   ) {
     // The serverOpts are mostly HTTPS-related options, so use `https` if there's any options set
-    server = https.createServer(config.webPortalConfig?.serverOpts, app);
+    server = https.createServer({
+        key: fs.readFileSync(config.webPortalConfig.serverOpts.key),
+        cert: fs.readFileSync(config.webPortalConfig.serverOpts.cert)
+    }, app);
   } else {
     // Otherwise, we just use `http`. This is pretty much the first half of `app.listen()`
     server = http.createServer(app);
